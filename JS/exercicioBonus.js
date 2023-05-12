36489dcaaa5044270c51f63b3290404f365fdb28
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-05-11 21:58:43 -0300

    ajustado identação e removido um print que ficou no código

--- JS/exercicioBonus.js
+++ JS/exercicioBonus.js
@@ -1,48 +1,47 @@
 // Exercicio Bônus
 
-async function a(){
-    b();
-    await c();
-    await d();
-    alertUser("a")
+async function a() {
+  b();
+  await c();
+  await d();
+  alertUser("a");
 }
 a();
 
-function b(){
-    return;
-    alertUser("b")
+function b() {
+  return;
+  alertUser("b");
 }
 
-function c(){
-    return new Promise((resolve) => {
-        resolve()
-        alertUser("c")
-    });
+function c() {
+  return new Promise((resolve) => {
+    resolve();
+    alertUser("c");
+  });
 }
 
-function d(){
-    return new Promise((resolve) => {
-        resolve()
-        alertUser("d")
-        console.log('passei o alert')
-    })
+function d() {
+  return new Promise((resolve) => {
+    resolve();
+    alertUser("d");
+  });
 }
 
-function alertUser(message){
-    console.log('A função é: '+message)
+function alertUser(message) {
+  console.log("A função é: " + message);
 }
 
 // Bônus:
 //     3) Qual a ordem dos prints no console?
-        // A saída no console é a seguinte:
-        // A função é: c
-        // A função é: d
-        // A função é: a
+// A saída no console é a seguinte:
+// A função é: c
+// A função é: d
+// A função é: a
 
 //      4) Existe algum erro nesse código? Se sim, comente sobre?
-        // Sim, há alguns erros no script, são eles:
-        // A função b está chamando o return antes de alertUser(), fazendo com que a função retorne imediatamente,
-        // assim o alertUser() nunca será executado
+// Sim, há alguns erros no script, são eles:
+// A função b está chamando o return antes de alertUser(), fazendo com que a função retorne imediatamente,
+// assim o alertUser() nunca será executado
 
-        // Nas funções 'c' e 'd' a promise está sendo resolvida antes da função alertUser() ser chamada. O correto
-        // seria chamar as funções que precisam ser executadas antes de resolver uma promise.
+// Nas funções 'c' e 'd' a promise está sendo resolvida antes da função alertUser() ser chamada. O correto
+// seria chamar as funções que precisam ser executadas antes de resolver uma promise.
